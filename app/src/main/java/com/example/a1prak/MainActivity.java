0e0fb7b634d101604662967b8e0bb3f3c21e03a0
wJexson <timur1704200311@gmail.com>
2023-02-24 20:33:21 +0300

    first commit

---
+++ app/src/main/java/com/example/a1prak/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.a1prak;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    int duration = Toast.LENGTH_SHORT;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "onCreate", duration).show();
+        Log.i(TAG, "onCreate");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Toast.makeText(MainActivity.this, "onStart", duration).show();
+        Log.v(TAG, "onStart");
+    }
+}
