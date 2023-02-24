ff7af391a956f92872c79e773fc272c3361b14a5
wJexson <timur1704200311@gmail.com>
2023-02-24 20:53:58 +0300

    finish

--- app/src/main/java/com/example/a1prak/MainActivity.java
+++ app/src/main/java/com/example/a1prak/MainActivity.java
@@ -16,4 +16,51 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, "onCreate", duration).show();
         Log.i(TAG, "onCreate");
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(MainActivity.this, "onStart", duration).show();
+        Log.v(TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Toast.makeText(MainActivity.this, "onRestart", duration).show();
+        Log.e(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(MainActivity.this, "onResume", duration).show();
+        Log.w(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(MainActivity.this, "onPause", duration).show();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Toast.makeText(MainActivity.this, "onStop", duration).show();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(MainActivity.this, "onDestroy", duration).show();
+        Log.e(TAG, "onDestroy");
+    }
 }
